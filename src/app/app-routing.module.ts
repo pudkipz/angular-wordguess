0bdab3f738ecf93afe2dd47c1aa8b2adce4b3388
pudkipz <jippie95@hotmail.com>
2025-02-15 16:36:30 +0100

    Setup things

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewComponent } from './game/view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ViewComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
