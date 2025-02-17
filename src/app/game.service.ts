dc1a66f3fd9c99adc91072f525d254e5584df15c
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-17 14:34:44 +0100

    Use words list to get secret word

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
+import { WORDS } from '../data/words';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  guessedWords: string[] = ['abcde'];
+  guessedWords: string[] = [];
   secret: string = 'hello';
   gameState: string = 'active';
+
+  constructor() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.secret = WORDS[Math.floor(Math.random() * WORDS.length)];
+    console.log(this.secret)
+    // this.secret = 'helpp';
+  }
 
   guessWord(word: string): boolean {
     if (this.guessedWords.includes(word)) {
